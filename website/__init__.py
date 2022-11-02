af66e176aa772d8a8df13f2b18945e5471badb51
Manish Kalyan <manishkalyan141@gmail.com>
2022-11-02 14:51:08 +0530

    second commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask ,Blueprint
+
 def create_app():
-  app=Flask('__name__')
+  app=Flask(__name__)
   app.config['SECRET_KEY']="manishitis"
+
+  from .auth import auth
+  from .views import views
+
+  app.register_blueprint(views,url_prefix="/")
+  app.register_blueprint(auth,url_prefix="/")
+
   return app
