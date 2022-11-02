739d882be30ff566d5fb00069fd8ad9cc5ea03fe
Manish Kalyan <manishkalyan141@gmail.com>
2022-11-02 22:47:25 +0530

    2nd update

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint,render_template,request,flash
 
 auth=Blueprint('auth',__name__)
 
-@auth.route("/login")
+@auth.route("/login",methods=['GET','POST'])
 def login():
-    return "<h1>login</h1>"
+    data=request.form
+    email=data.get('email')
+    pwd=data.get('password')
+    return render_template("login.html")
 
-@auth.route("/logout")
+@auth.route("/logout",methods=['GET','POST'])
 def logout():
-    return "<h1>logout</h1>"
+    return render_template("home.html")
 
-@auth.route("/signup")
+@auth.route("/signup",methods=['GET','POST'])
 def signup():
-    return "<h1>sign up</h1>"
+    if request.method=='POST':
+        data=request.form
+        email=data['email']
+        name =data['name']
+        pwd1= data['password1']
+        pwd2= data['password2']
+
+        if len(email)<4:
+            flash("email must be valid",category="error")
+        elif len(name)<2:
+            flash("name should be greater than 1 character",category="error")
+        elif len(pwd1)<7:
+            flash("length of password must be greater than 6 characters",category="error")
+        elif pwd1 != pwd2:
+            flash("password should be same",category="error")
+        else:
+            flash("account created",category="success")
+    return render_template("signup.html")
